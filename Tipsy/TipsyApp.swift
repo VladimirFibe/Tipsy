3e38f988d9613484982be51e77a4f14a1886d306
Vladimir Fibe <motiw@icloud.com>
2022-02-16 23:36:19 +0600

    Initial Commit

---
+++ Tipsy/TipsyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipsyApp.swift
+//  Tipsy
+//
+//  Created by Vladimir Fibe on 16.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct TipsyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
