2505e552e30949cab13fbafe36eafb1fd7d6dd59
Vladimir Fibe <motiw@icloud.com>
2023-02-19 18:31:21 +0600

    8.png

--- README.md
+++ README.md
@@ -24,9 +24,54 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 ![](/img/2.png)
 
-Начнем с цвета фона
+Начнем с цвета фонаюююю
 
 ![](/img/3.png)
 
 Перетягиваем цвет в код
 ![](/img/4.png)
+
+![](/img/5.png)
+
+![](/img/6.png)
+
+```swift
+    let enterBillTotal = UILabel()
+    let billTextField = UITextField()
+    lazy var titleStack = UIStackView(arrangedSubviews: [enterBillTotal, billTextField])
+```
+
+```swift
+    func setupViews() {
+        view.addSubview(titleStack)
+    }
+```
+
+```swift
+    func layoutViews() {
+        titleStack.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            titleStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
+            view.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: titleStack.trailingAnchor, constant: 50)
+        ])
+    }
+```
+
+```swift
+    func configureAppearance() {
+        configureColos()
+        enterBillTotal.text = "Enter bill total"
+        enterBillTotal.font = .systemFont(ofSize: 25)
+        
+        billTextField.placeholder = "e.g. 12356"
+        billTextField.textAlignment = .center
+        billTextField.font = .systemFont(ofSize: 40)
+        
+        titleStack.axis = .vertical
+        titleStack.spacing = 26
+    }
+```
+![](/img/8.png)
+
+![](/img/7.png)
--- Tipsy/CalculatorViewController.swift
+++ Tipsy/CalculatorViewController.swift
@@ -1,6 +1,9 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
+    let enterBillTotal = UILabel()
+    let billTextField = UITextField()
+    lazy var titleStack = UIStackView(arrangedSubviews: [enterBillTotal, billTextField])
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
@@ -11,14 +14,35 @@ class CalculatorViewController: UIViewController {
 
 extension CalculatorViewController {
     func setupViews() {
+        view.addSubview(titleStack)
     }
     
     func layoutViews() {
+        titleStack.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
+            titleStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            titleStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
+            view.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: titleStack.trailingAnchor, constant: 50)
         ])
     }
     
     func configureAppearance() {
+        configureColos()
+        enterBillTotal.text = "Enter bill total"
+        enterBillTotal.font = .systemFont(ofSize: 25)
+        enterBillTotal.textColor = .lightGray
+        
+        billTextField.placeholder = "e.g. 12356"
+        billTextField.textAlignment = .center
+        billTextField.font = .systemFont(ofSize: 40)
+        
+        titleStack.axis = .vertical
+        titleStack.spacing = 26
+    }
+    
+    func configureColos() {
         view.backgroundColor = #colorLiteral(red: 0.9776372313, green: 0.9993813634, blue: 0.9938063025, alpha: 1)
+        enterBillTotal.textColor = .lightGray
+        billTextField.textColor = #colorLiteral(red: 0, green: 0.6901960784, blue: 0.4196078431, alpha: 1)
     }
 }
